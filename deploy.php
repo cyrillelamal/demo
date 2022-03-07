e4809f312b7188969e6a42bb1d40711b8fb82a6e
Cyrille <horoshijavtobus@yandex.ru>
2022-03-07 19:27:33 +0300

    deploy

--- deploy.php
+++ deploy.php
@@ -1,7 +1,7 @@
 <?php
 namespace Deployer;
 
-require 'recipe/symfony4.php';
+require 'recipe/symfony.php';
 
 // Project name
 set('application', 'Demo');
@@ -17,11 +17,9 @@ add('shared_dirs', []);
 // Writable dirs by web server
 add('writable_dirs', []);
 
-
 // Hosts
-
 host('80.249.145.124')
-   ->set('remote_user', 'root')
+   ->set('remote_user', 'www-data')
    ->set('deploy_path', '/var/www/demo')
    ->user('root')
    ->identityFile('~/.ssh/key')
@@ -29,10 +27,29 @@ host('80.249.145.124')
 
 // Tasks
 desc('Deploy');
-task('deploy', function () {
+task('deploy', [
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:clear_paths',
+    'deploy:create_cache_dir',
+    'deploy:shared',
+    'deploy:assets',
+    'dependencies:install',
+    'deploy:cache:clear',
+    'deploy:cache:warmup',
+    'deploy:writable',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
+
+task('dependencies:install', function() {
     run('export APP_ENV=prod');
     run('/usr/local/bin/composer install --no-dev');
 });
+
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
