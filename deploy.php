b283d6468c4aa2b90ef51e141a96167fe3e9b084
Cyrille <horoshijavtobus@yandex.ru>
2022-03-07 18:08:13 +0300

    dep test

---
+++ deploy.php
@@ -0,0 +1,36 @@
+<?php
+namespace Deployer;
+
+require 'recipe/symfony.php';
+
+// Project name
+set('application', 'Demo');
+
+// Project repository
+set('repository', 'git@github.com:cyrillelamal/demo.git');
+
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('80.249.145.124')
+   ->set('remote_user', 'root')
+   ->set('deploy_path', '/var/www/demo')
+   ->user('root')
+   ->identityFile('~/.ssh/key');
+
+// Tasks
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'database:migrate');
